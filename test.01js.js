c1d06038ad2f8883f9f830aef081731e64f1abbd
6xd <ldtheended@163.com>
2020-11-02 18:21:50 +0800

    a0.02（nodejs 处理get请求）

--- test.01js.js
+++ test.01js.js
@@ -1,6 +1,15 @@
 const http = require('http')
+const querystring = require('querystring')
 
 const server = http.createServer((req, res) => {
-    res.end("hello world");
-});
-server.listen(8000);
+    const method = req.method
+    console.log('method: ', method)
+    const url = req.url
+    console.log('url: ', url)
+    req.query = querystring.parse(url.split('?')[1])  // 解析 querystring
+    console.log('query: ', req.query)
+    res.end(JSON.stringify(req.query)); // 将 querystring 返回
+})
+
+server.listen(8000)
+console.log('OK')
