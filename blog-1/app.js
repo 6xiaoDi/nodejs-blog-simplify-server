7d490d29c65bdb6845a826c07e41a4885f4a221e
6xd <ldtheended@163.com>
2020-11-02 23:04:54 +0800

    a0.09（初始化路由——测试接口是否调通）

--- blog-1/app.js
+++ blog-1/app.js
@@ -1,16 +1,33 @@
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
+
 const serverHandle = (req, res) => {
     // 设置返回格式 JSON
-    res.setHeader('Content-type', 'application/json')
+    res.setHeader('Content-type', 'application/json');
 
-    const resData = {
-        name: '6xiaodi',
-        site: 'https://me.csdn.net/u013946061',
-        env: process.env.NODE_ENV
+    // 处理 blog 路由
+    const blogData = handleBlogRouter(req, res);
+    if (blogData) {
+        res.end (
+            JSON.stringify(blogData)
+        )
+        return;
     }
 
-    res.end(
-        JSON.stringify(resData)
-    )
+    // 处理 user 路由
+    const userData = handleUserRouter(req, res);
+    if (userData) {
+        res.end (
+            JSON.stringify(userData)
+        )
+        return;
+    }
+
+    // 未命中路由，返回404
+    res.writeHead(404, {"Content-tupe": "text/plain"}); // 写上状态码404。修改Head为 "text/plain"，即纯文本
+    res.write("404 Not Found\n"); // 写入内容
+    res.end();
 }
 
-module.exports = serverHandle
+module.exports = serverHandle;
+// process.env.NODE_ENV
---
+++ blog-1/src/router/blog.js
@@ -0,0 +1,42 @@
+const handleBlogRouter = (req, res) => {
+    const method = req.method // GET POST
+    const url = req.url;
+    const path = url.split('?')[0];
+
+    // 获取博客列表
+    if (method === 'GET' && path === '/api/blog/list') {
+        return {
+            msg: '这是获取博客列表的接口'
+        }
+    }
+
+    // 获取博客详情
+    if (method === 'GET' && path === '/api/blog/detail') {
+        return {
+            msg: '这是获取博客详情的接口'
+        }
+    }
+
+    // 新建一篇博客
+    if (method === 'POST' && path === '/api/blog/new') {
+        return {
+            msg: '这是新建博客详情的接口'
+        }
+    }
+
+    // 更新一篇博客
+    if (method === 'POST' && path === '/api/blog/update') {
+        return {
+            msg: '这是更新博客详情的接口'
+        }
+    }
+
+    // 删除一篇博客
+    if (method === 'POST' && path === '/api/blog/delete') {
+        return {
+            msg: '这是删除博客详情的接口'
+        }
+    }
+}
+
+module.exports = handleBlogRouter;
