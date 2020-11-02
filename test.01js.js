c000c36fcee66b0ba90f61c31d124feda34a0e1e
6xd <ldtheended@163.com>
2020-11-02 18:25:41 +0800

    a0.04（nodejs处理路由）

--- test.01js.js
+++ test.01js.js
@@ -1,23 +1,29 @@
 const http = require('http')
 
+// const server = http.createServer((req, res) => {
+//     if (req.method === 'POST') {
+//         // req 数据格式
+//         console.log('req content-type', req.headers['content-type']);
+//         // 接收数据（数据流的方式）
+//         let postData = ''
+//         req.on('data', chunk => {
+//             postData += chunk.toString()
+//         })
+//         // 数据流完，触发end事件
+//         req.on('end', () => {
+//             console.log(postData)
+//             // 在这里返回，因为是异步
+//             res.end(
+//                 'hello world'
+//             )
+//         })
+//     }
+// })
+
 const server = http.createServer((req, res) => {
-    if (req.method === 'POST') {
-        // req 数据格式
-        console.log('req content-type', req.headers['content-type']);
-        // 接收数据（数据流的方式）
-        let postData = ''
-        req.on('data', chunk => {
-            postData += chunk.toString()
-        })
-        // 数据流完，触发end事件
-        req.on('end', () => {
-            console.log(postData)
-            // 在这里返回，因为是异步
-            res.end(
-                'hello world'
-            )
-        })
-    }
+    const url = req.url
+    const path = url.split('?')[0]
+    res.end(path); //返回路由
 })
 
 server.listen(8000)
