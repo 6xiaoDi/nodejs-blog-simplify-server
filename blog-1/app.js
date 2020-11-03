01eea25428c7716eced320bf5031de70c5dc71c9
6xd <ldtheended@163.com>
2020-11-03 18:10:17 +0800

    a0.17（开发路由——处理 POSTData）

--- blog-1/app.js
+++ blog-1/app.js
@@ -2,6 +2,38 @@ const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 const querystring = require('querystring')
 
+// 用于处理 post data
+const getPostData = (req) => {
+    const promise = new Promise((resolve, reject) => {
+        // get 请求没有post data
+        if (req.method !== 'POST') {
+            resolve({})
+            return
+        }
+        // 数据格式不是json
+        if (req.headers['content-type'] !== 'application/json') {
+            resolve({})
+            return
+        }
+        // 是post请求
+        let postData = ''
+        req.on('data', chunk => {
+            postData += chunk.toString()
+        })
+        req.on('end', () => {
+            // 没有数据
+            if (!postData) {
+                resolve({})
+                return
+            }
+            resolve(
+                JSON.parse(postData)
+            )
+        })
+    })
+    return promise
+}
+
 const serverHandle = (req, res) => {
     // 设置返回格式 JSON
     res.setHeader('Content-type', 'application/json');
@@ -13,28 +45,33 @@ const serverHandle = (req, res) => {
     // 解析 query
     req.query = querystring.parse(url.split('?')[1])
 
-    // 处理 blog 路由
-    const blogData = handleBlogRouter(req, res);
-    if (blogData) {
-        res.end (
-            JSON.stringify(blogData)
-        )
-        return;
-    }
+    // 处理postdata
+    getPostData(req).then(postData => {
+        req.body = postData;
 
-    // 处理 user 路由
-    const userData = handleUserRouter(req, res);
-    if (userData) {
-        res.end (
-            JSON.stringify(userData)
-        )
-        return;
-    }
+        // 处理 blog 路由
+        const blogData = handleBlogRouter(req, res);
+        if (blogData) {
+            res.end (
+                JSON.stringify(blogData)
+            )
+            return;
+        }
 
-    // 未命中路由，返回404
-    res.writeHead(404, {"Content-tupe": "text/plain"}); // 写上状态码404。修改Head为 "text/plain"，即纯文本
-    res.write("404 Not Found\n"); // 写入内容
-    res.end();
+        // 处理 user 路由
+        const userData = handleUserRouter(req, res);
+        if (userData) {
+            res.end (
+                JSON.stringify(userData)
+            )
+            return;
+        }
+
+        // 未命中路由，返回404
+        res.writeHead(404, {"Content-tupe": "text/plain"}); // 写上状态码404。修改Head为 "text/plain"，即纯文本
+        res.write("404 Not Found\n"); // 写入内容
+        res.end();
+    });
 }
 
 module.exports = serverHandle;
