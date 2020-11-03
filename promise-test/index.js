e436ce22a7483c514c2b929f9fa78f9d2079b4b2
6xd <ldtheended@163.com>
2020-11-03 14:32:25 +0800

    a0.14（promise——异步读取文件封装成回调）

--- promise-test/index.js
+++ promise-test/index.js
@@ -1,12 +1,21 @@
 const fs = require('fs')
 const path = require('path')
 
-const fullFileName = path.resolve(__dirname, 'files', 'a.json');
-fs.readFile(fullFileName, (err, data) => {
-    if (err) {
-        console.error(err)
-        return
-    }
-    console.log(data.toString())
-})
+// callback 方式获取一个文件的内容
+function getFileContent(fileName, callback) {
+    const fullFileName = path.resolve(__dirname, 'files', fileName)
+    fs.readFile(fullFileName, (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        callback(
+            JSON.parse(data.toString())
+        )
+    })
+}
 
+// 测试
+getFileContent('a.json', aData => {
+    console.log('a data => ', aData);
+})
