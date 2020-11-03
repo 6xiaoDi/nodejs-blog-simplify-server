c852aaf2cd83b2d6cb34979d90f21c958cddf68d
6xd <ldtheended@163.com>
2020-11-03 14:43:52 +0800

    a0.16（promise——异步读取文件封装成回调，通过next继续往下读取文件 => promise优化）

--- promise-test/index.js
+++ promise-test/index.js
@@ -2,26 +2,53 @@ const fs = require('fs')
 const path = require('path')
 
 // callback 方式获取一个文件的内容
-function getFileContent(fileName, callback) {
-    const fullFileName = path.resolve(__dirname, 'files', fileName)
-    fs.readFile(fullFileName, (err, data) => {
-        if (err) {
-            console.error(err)
-            return
-        }
-        callback(
-            JSON.parse(data.toString())
-        )
-    })
-}
+// function getFileContent(fileName, callback) {
+//     const fullFileName = path.resolve(__dirname, 'files', fileName)
+//     fs.readFile(fullFileName, (err, data) => {
+//         if (err) {
+//             console.error(err)
+//             return
+//         }
+//         callback(
+//             JSON.parse(data.toString())
+//         )
+//     })
+// }
 
-// 测试
-getFileContent('a.json', aData => {
-    console.log('a data => ', aData);
-    getFileContent(aData.next, bData => {
-        console.log('b data => ', bData);
-        getFileContent(bData.next, cData => {
-            console.log('c data => ', cData);
+// 测试 callback-hell
+// getFileContent('a.json', aData => {
+//     console.log('a data => ', aData);
+//     getFileContent(aData.next, bData => {
+//         console.log('b data => ', bData);
+//         getFileContent(bData.next, cData => {
+//             console.log('c data => ', cData);
+//         })
+//     })
+// })
+
+// 用 promise 获取文件内容
+function getFileContent(fileName) {
+    const promise = new Promise((resolve, reject) => {
+        const fullFileName = path.resolve(__dirname, 'files', fileName)
+        fs.readFile(fullFileName, (err, data) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            resolve(
+                JSON.parse(data.toString())
+            )
         })
     })
+    return promise
+}
+
+getFileContent('a.json').then(aData => {
+    console.log('a data', aData)
+    return getFileContent(aData.next)
+}).then(bData => {
+    console.log('b data', bData)
+    return getFileContent(bData.next)
+}).then(cData => {
+    console.log('c data', cData)
 })
