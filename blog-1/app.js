cd69e332b92f64f6342b59e2e0cdd848ae91a99e
6xd <ldtheended@163.com>
2020-11-05 11:34:10 +0800

    a0.26（API对接mysql——博客列表）

--- blog-1/app.js
+++ blog-1/app.js
@@ -50,12 +50,21 @@ const serverHandle = (req, res) => {
         req.body = postData;
 
         // 处理 blog 路由
-        const blogData = handleBlogRouter(req, res);
-        if (blogData) {
-            res.end (
-                JSON.stringify(blogData)
-            )
-            return;
+        // const blogData = handleBlogRouter(req, res);
+        // if (blogData) {
+        //     res.end (
+        //         JSON.stringify(blogData)
+        //     )
+        //     return;
+        // }
+        const blogResult = handleBlogRouter(req, res)
+        if (blogResult) {
+            blogResult.then(blogData => {
+                res.end(
+                    JSON.stringify(blogData)
+                )
+            })
+            return
         }
 
         // 处理 user 路由
--- blog-1/src/controller/blog.js
+++ blog-1/src/controller/blog.js
@@ -1,21 +1,17 @@
+const { exec } = require('../db/mysql')
+
 const  getList = (author, keyword) => {
-    // 先返回假数据（格式是正确的）
-    return [
-        {
-            id: 1,
-            title: "标题A",
-            content: "内容A",
-            createTime: 1604368609540,
-            author:"zhangsan"
-        },
-        {
-            id: 2,
-            title: "标题B",
-            content: "内容B",
-            createTime: 1604368769975,
-            author:"lisi"
-        }
-    ]
+    let sql = `select * from blogs where 1=1 `
+    if (author) {
+        sql += `and author='${author}' `
+    }
+    if (keyword) {
+        sql += `and title like '%${keyword}%' `
+    }
+    sql += `order by createtime desc;`
+
+    // 返回 promise
+    return exec(sql)
 }
 
 const getDetail = (id) => {
