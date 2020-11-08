ec1aeb24a03ab3a1879caf88b220fc821d409a1c
6xd <ldtheended@163.com>
2020-11-08 13:24:39 +0800

    a0.49（stream—— post 采用流的方式输出内容  =>  pipe管道方式）

--- stream-test/test1.js
+++ stream-test/test1.js
@@ -1,2 +1,10 @@
 // 标准输入输出
-process.stdin.pipe(process.stdout)
+// process.stdin.pipe(process.stdout)
+
+const http = require('http')
+const server = http.createServer((req, res) => {
+    if (req.method === 'POST') {
+        req.pipe(res)  // 最主要
+    }
+})
+server.listen(8050)
