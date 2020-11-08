1a1986726839c0b533070ccbcaa7c294cb6731bf
6xd <ldtheended@163.com>
2020-11-08 23:58:39 +0800

    a0.56（安全—— 预防sql注入）

--- blog-1/src/controller/user.js
+++ blog-1/src/controller/user.js
@@ -1,11 +1,13 @@
-const { exec } = require('../db/mysql')
+const { exec, escape } = require('../db/mysql')
 
 const login = (username, password) => {
+    username = escape(username);
+    password = escape(password);
 
     const sql = `
-        select username, realname from users where username='${username}' and password='${password}'
+        select username, realname from users where username=${username} and password=${password}
     `
-    // console.log('sql is', sql)
+    console.log('sql is', sql)
     return exec(sql).then(rows => {
         // 兼容性处理，为空则返回空对象
         return rows[0] || {}
