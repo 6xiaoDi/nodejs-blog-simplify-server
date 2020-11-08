136ec8b236fa9fbab49f21a9045afcc5713d3af2
6xd <ldtheended@163.com>
2020-11-08 13:45:54 +0800

    a0.52（stream—— get请求，用流的方式返回数据  ）

--- stream-test/test1.js
+++ stream-test/test1.js
@@ -10,26 +10,40 @@
 // server.listen(8050)
 
 // 复制文件
+// const fs = require('fs')
+// const path = require('path')
+//
+// // 两个文件名
+// const fileName1 = path.resolve(__dirname, 'data.txt')
+// const fileName2 = path.resolve(__dirname, 'data-bak.txt')
+//
+// // 读取文件的stream对象
+// const readStream = fs.createReadStream(fileName1)
+// // 写入文件的stream对象
+// const writeStream = fs.createWriteStream(fileName2)
+// // 执行拷贝,通过pipe
+// readStream.pipe(writeStream)
+//
+// // 监视数据转存过程，即每一次读取的内容
+// readStream.on('data', chunk => {
+//     console.log('监听过程：'+chunk.toString())
+// })
+//
+// // 数据读取完成,即拷贝完成
+// readStream.on('end', () => {
+//     console.log('copy done')
+// })
+
+
+const http = require('http')
 const fs = require('fs')
 const path = require('path')
 
-// 两个文件名
 const fileName1 = path.resolve(__dirname, 'data.txt')
-const fileName2 = path.resolve(__dirname, 'data-bak.txt')
-
-// 读取文件的stream对象
-const readStream = fs.createReadStream(fileName1)
-// 写入文件的stream对象
-const writeStream = fs.createWriteStream(fileName2)
-// 执行拷贝,通过pipe
-readStream.pipe(writeStream)
-
-// 监视数据转存过程，即每一次读取的内容
-readStream.on('data', chunk => {
-    console.log('监听过程：'+chunk.toString())
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET') {
+        const readStream = fs.createReadStream(fileName1)
+        readStream.pipe(res) // 将res作为stream的dest
+    }
 })
-
-// 数据读取完成,即拷贝完成
-readStream.on('end', () => {
-    console.log('copy done')
-})
+server.listen(8050)
