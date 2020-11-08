97437e9cfcfbb6aef2392b80f650400080545d6d
6xd <ldtheended@163.com>
2020-11-08 11:55:15 +0800

    a0.47（nodejs文件操作——判断文件是否存在）

--- file-test/test1.js
+++ file-test/test1.js
@@ -14,12 +14,17 @@ const fileName = path.resolve(__dirname, 'data.txt')
 // })
 
 // 写入文件
-const content = '这是新写入的内容\n'
-const opt = {
-    flag: 'a'  // 追加写入。覆盖用 'w'
-}
-fs.writeFile(fileName, content, opt, (err) => {
-    if (err) {
-        console.error(err)
-    }
+// const content = '这是新写入的内容\n'
+// const opt = {
+//     flag: 'a'  // 追加写入。覆盖用 'w'
+// }
+// fs.writeFile(fileName, content, opt, (err) => {
+//     if (err) {
+//         console.error(err)
+//     }
+// })
+
+// 判断文件是否存在
+fs.exists(fileName, (exist) => {
+    console.log('exist', exist)
 })
