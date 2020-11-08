4850e810b6fbe868171c6a4708c4a5ed33c6607e
6xd <ldtheended@163.com>
2020-11-08 20:53:13 +0800

    a0.53（写日志  ）

--- blog-1/app.js
+++ blog-1/app.js
@@ -1,5 +1,6 @@
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
+const { access } = require('./src/utils/log')
 const querystring = require('querystring')
 const { get, set } = require('./src/db/redis')
 
@@ -48,6 +49,9 @@ const getPostData = (req) => {
 }
 
 const serverHandle = (req, res) => {
+    // 记录 access log
+    access(`${req.method} -- ${req.url} -- ${req.headers['user-agent']} -- ${Date.now()}`)
+
     // 设置返回格式 JSON
     res.setHeader('Content-type', 'application/json');
 
