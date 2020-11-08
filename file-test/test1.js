b9bd42c754be5fb056da38be5ed6d47787a87daa
6xd <ldtheended@163.com>
2020-11-08 11:51:32 +0800

    a0.46（nodejs文件操作——写入文件）

--- file-test/test1.js
+++ file-test/test1.js
@@ -4,11 +4,22 @@ const path = require('path')
 const fileName = path.resolve(__dirname, 'data.txt')
 
 // 读取文件内容
-fs.readFile(fileName, (err, data) => {
+// fs.readFile(fileName, (err, data) => {
+//     if (err) {
+//         console.error(err)
+//         return
+//     }
+//     // data 是二进制类型，需要转换为字符串
+//     console.log(data.toString())
+// })
+
+// 写入文件
+const content = '这是新写入的内容\n'
+const opt = {
+    flag: 'a'  // 追加写入。覆盖用 'w'
+}
+fs.writeFile(fileName, content, opt, (err) => {
     if (err) {
         console.error(err)
-        return
     }
-    // data 是二进制类型，需要转换为字符串
-    console.log(data.toString())
 })
