62e77386b88d64b799d9f7e35d80655b594c6b46
6xd <ldtheended@163.com>
2020-11-10 17:03:35 +0800

    a0.64（express重构项目——express路由之post请求）

--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -5,7 +5,14 @@ router.get('/list', (req, res, next) => {
     res.json({
         errno: 0,
         data: [1, 2, 3]
-    })
+    });
+});
+
+router.get('/detail', (req, res, next) => {
+    res.json({
+        errno: 0,
+        data: 'ok'
+    });
 });
 
 module.exports = router;
