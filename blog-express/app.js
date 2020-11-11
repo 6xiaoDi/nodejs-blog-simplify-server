86f837f503b1326042f3a376eedebe0a3991f40b
6xd <ldtheended@163.com>
2020-11-11 17:43:01 +0800

    a0.77（morgan——日志——stream默认参数）

--- blog-express/app.js
+++ blog-express/app.js
@@ -17,7 +17,9 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger('dev', {
+  stream:process.stdout
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
