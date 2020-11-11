d127df5d35c4914e7809b669385954c7664d0fb7
6xd <ldtheended@163.com>
2020-11-11 16:50:20 +0800

    a0.76（完成blog后端接口）

--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -7,6 +7,7 @@ const {
     delBlog
 } = require('../controller/blog')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
+const loginCheck = require('../middleware/loginCheck')
 
 var router = express.Router();
 
@@ -38,10 +39,53 @@ router.get('/list', (req, res, next) => {
 });
 
 router.get('/detail', (req, res, next) => {
-    res.json({
-        errno: 0,
-        data: 'ok'
-    });
+    const result = getDetail(req.query.id)
+    return result.then(data => {
+        res.json(
+            new SuccessModel(data)
+        )
+    })
 });
+
+router.post('/new', loginCheck, (req, res, next) => {
+    req.body.author = req.session.username
+    const result = newBlog(req.body)
+    return result.then(data => {
+        res.json(
+            new SuccessModel(data)
+        )
+    })
+})
+
+router.post('/update', loginCheck, (req, res, next) => {
+    const result = updateBlog(req.query.id, req.body)
+    return result.then(val => {
+        if (val) {
+            res.json(
+                new SuccessModel('更新博客成功')
+            )
+        } else {
+            res.json(
+                new ErrorModel('更新博客失败')
+            )
+        }
+    })
+})
+
+router.post('/del', loginCheck, (req, res, next) => {
+    const author = req.session.username
+    const result = delBlog(req.query.id, author)
+    return result.then(val => {
+        if (val) {
+            res.json(
+                new SuccessModel("删除博客成功")
+            )
+        } else {
+            res.json(
+                new ErrorModel('删除博客失败')
+            )
+        }
+    })
+})
 
 module.exports = router;
