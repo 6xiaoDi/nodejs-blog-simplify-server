91d7220654e3af1ade24303531766df7d7c4df60
6xd <ldtheended@163.com>
2020-11-11 14:20:46 +0800

    a0.73（express处理session——实现登录）

--- blog-express/routes/user.js
+++ blog-express/routes/user.js
@@ -1,28 +1,53 @@
 var express = require('express');
 var router = express.Router();
+const { login } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 
-router.post('/login', (req, res, next) => {
-    const { username, password } = req.body;
-    res.json({
-        errno: 0,
-        data: {
-            username,
-            password
+router.post('/login', function(req, res, next) {
+    const { username, password } = req.body
+    const result = login(username, password)
+    return result.then(data => {
+        if (data.username) {
+            // 设置 session
+            req.session.username = data.username
+            req.session.realname = data.realname
+
+            res.json(
+                new SuccessModel()
+            )
+            return
         }
-    });
+        res.json(
+            new ErrorModel('登录失败')
+        )
+    })
 });
 
-router.get('/session-test', (req, res, next) => {
-    const session = req.session
-    // session记录浏览次数
-    if (session.viewNum == null) {
-        session.viewNum = 0
+router.get('/login-test', (req, res, next) => {
+    if (req.session.username) {
+        res.json({
+            errno: 0,
+            msg: '已登录'
+        })
+        return
     }
-    session.viewNum++
-
     res.json({
-        viewNum: session.viewNum
+        errno: -1,
+        msg: '未登录'
     })
 })
+
+// router.get('/session-test', (req, res, next) => {
+//     const session = req.session
+//     // session记录浏览次数
+//     if (session.viewNum == null) {
+//         session.viewNum = 0
+//     }
+//     session.viewNum++
+//
+//     res.json({
+//         viewNum: session.viewNum
+//     })
+// })
 
 module.exports = router;
