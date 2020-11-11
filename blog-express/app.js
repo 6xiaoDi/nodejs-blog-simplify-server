7fcd2aaa1bdb93e895215000d0c8f6f420ddc128
6xd <ldtheended@163.com>
2020-11-11 13:53:26 +0800

    a0.71（express处理session——配置）

--- blog-express/app.js
+++ blog-express/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -20,6 +21,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'WJiol#23123_',
+  cookie: {
+    // path: '/',   // 默认配置
+    // httpOnly: true,  // 默认配置
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}))
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
