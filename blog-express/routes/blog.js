4ba1515427151e1a3420656f4536def0e366cb3d
6xd <ldtheended@163.com>
2020-11-11 10:10:55 +0800

    a0.70（express开发博客项目-初始化环境——blog路由中获取列表功能）

--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -1,11 +1,25 @@
 var express = require('express');
+const {
+    getList,
+    getDetail,
+    newBlog,
+    updateBlog,
+    delBlog
+} = require('../controller/blog')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+
 var router = express.Router();
 
 router.get('/list', (req, res, next) => {
-    res.json({
-        errno: 0,
-        data: [1, 2, 3]
-    });
+    let author = req.query.author || ''
+    const keyword = req.query.keyword || ''
+
+    const result = getList(author, keyword)
+    return result.then(listData => {
+        res.json(
+            new SuccessModel(listData)
+        )
+    })
 });
 
 router.get('/detail', (req, res, next) => {
