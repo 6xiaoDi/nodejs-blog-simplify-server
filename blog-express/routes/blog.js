0d525a43f700d8c32f98182e141afb3a569ee871
6xd <ldtheended@163.com>
2020-11-11 23:10:23 +0800

    a0.80（morgan——日志——实现prd和dev模式下的日志）

--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -16,10 +16,10 @@ router.get('/list', (req, res, next) => {
     const keyword = req.query.keyword || ''
 
     if (req.query.isadmin) {
-        // console.log('is admin')
+        console.log('is admin')
         // 管理员界面
         if (req.session.username == null) {
-            // console.error('is admin, but no login')
+            console.error('is admin, but no login')
             // 未登录
             res.json(
                 new ErrorModel('未登录')
