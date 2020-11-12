ad0407526281589188cd205d9f283b116f37e0bd
6xd <ldtheended@163.com>
2020-11-12 23:06:31 +0800

    a0.87（blog路由）

--- blog-koa2/app.js
+++ blog-koa2/app.js
@@ -8,6 +8,7 @@ const logger = require('koa-logger')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
+const blog = require('./routes/blog')
 
 // error handler
 onerror(app)
@@ -35,6 +36,7 @@ app.use(async (ctx, next) => {
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+app.use(blog.routes(), blog.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
