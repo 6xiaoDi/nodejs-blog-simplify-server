7d9ca2bbad2ce89371867717fd5b1c6cd76b0372
6xd <ldtheended@163.com>
2020-11-12 14:24:09 +0800

    a0.84（async  /  await 语法——执行async包裹的函数，返回的还是一个promise对象）

--- async-test/index.js
+++ async-test/index.js
@@ -53,18 +53,28 @@ function getFileContent(fileName) {
 //     console.log('c data', cData)
 // })
 
-async function readFileData() {
-    // 同步写法
-    try {
-        const aData = await getFileContent('a.json')
-        console.log('a data', aData)
-        const bData = await getFileContent(aData.next)
-        console.log('b data', bData)
-        const cData = await getFileContent(bData.next)
-        console.log('c data', cData)
-    } catch (err) {
-        console.error(err)
-    }
-}
+// async function readFileData() {
+//     // 同步写法
+//     try {
+//         const aData = await getFileContent('a.json')
+//         console.log('a data', aData)
+//         const bData = await getFileContent(aData.next)
+//         console.log('b data', bData)
+//         const cData = await getFileContent(bData.next)
+//         console.log('c data', cData)
+//     } catch (err) {
+//         console.error(err)
+//     }
+// }
+//
+// readFileData()
 
-readFileData()
+async function readAData() {
+    const aData = await getFileContent('a.json')
+    return aData
+}
+async function test() {
+    const aData = await readAData()
+    console.log(aData)
+}
+test()
